03aca5124a03ff2080097b663fb60ed2ba76dfc1
ShawnBorn-Newton <shawn.newton3@gmail.com>
2017-08-21 12:47:54 -0400

    its numbers

--- SpeedReader/Program.cs
+++ SpeedReader/Program.cs
@@ -18,14 +18,54 @@ namespace SpeedReader
             using (StreamReader reader = new StreamReader(@"C:\Users\WeCanCodeIT\Documents\Visual Studio 2015\Projects\SpeedReader\DrivingRecord.txt"))
             {
                 List<string> list = new List<string>();
+
                 string line;
+
                 while ((line = reader.ReadLine()) != null)
                 {
                     list.Add(line);
 
-                    Console.WriteLine(line);
+                    //Console.WriteLine(line);
+
+                    //take out :
+                    string[] words = line.Split(' ');
+                    foreach (string word in words)
+                    {
+                        
+                        if(word.Contains(':'))
+                        {
+                            string[] numbers = word.Split(':');
+                            foreach( string number in numbers)
+                            {
+                                Console.WriteLine(number);
+                                //int mathNum = int.Parse(number);
+                            }  
+                        }
+                       // //take out chars
+                        //char[] letters =  word.ToCharArray();
+                        // foreach( char letter in letters)
+                        // {
+                        //     bool isNum = Char.IsNumber(letter);
+                        //     if(isNum == true)
+                        //     {
+                        //         Console.WriteLine(letter);
+                        //     }
+                    }
+                    }
+
                 }
+                //for (int i = 0; i <= word.Length - 1; i++)
+                //{
+                //    if (line[i] ==':')
+                //    {
+                //        line[i].Replace(':', '.');
+                //    }
+
+                //    Console.WriteLine(i);
+                //}
+
             }
         }
     }
-}
+
+
